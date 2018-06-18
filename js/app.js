a23417f126d75967a54a9c059a0e552fd2adc7e1
LoreVazquez <lorena_vor@hotmail.com>
2018-06-18 13:40:18 -0500

    Se agrega los apartados de los modales de inicio de sesion, registro y de nuevo conductor

--- js/app.js
+++ js/app.js
@@ -3,3 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {opacity: 0.5});
+  });
