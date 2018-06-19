d307c923d4532451f8f2580adaf790115b82c520
LoreVazquez <lorena_vor@hotmail.com>
2018-06-18 23:15:02 -0500

    Disemo del modal de registo y coductor, funcionalidad del menu de la versión para mobile, imagen en el titulo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-// La funcionalidad de tu proyecto
+//Evento para inicializar modales
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems, {opacity: 0.5});
+});
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {opacity: 0.5});
-  });
+// Evento para inicializar menu en versión móvil
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+//Evento para inicializar select
+$(document).ready(function(){
+  $('select').formSelect();
+});
